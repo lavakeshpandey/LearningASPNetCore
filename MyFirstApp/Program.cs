94e54f94444f6b8f6f298c4bf108d87220942704
Lavakesh Pandey <jsonbourneagain@outlook.com>
2025-07-09 18:55:50 +0530

    Route grouping

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -15,30 +15,35 @@ app.UseStatusCodePages();
 
 var _fruit = new ConcurrentDictionary<string, Fruit>();
 
-app.MapGet("/fruit", () => _fruit);
+RouteGroupBuilder fruitGroup = app.MapGroup("/fruit").AddEndpointFilter<IdValidationFilter>();
 
-app.MapGet("/fruit/{id}", (string id) =>
+fruitGroup.MapGet("/", () => _fruit);
+
+//RouteGroupBuilder fruitApiWithValidation = fruitGroup.MapGroup("/").AddEndpointFilter<IdValidationFilter>();
+
+//app.MapGet("/fruit", () => _fruit);
+
+fruitGroup.MapGet("/fruit/{id}", (string id) =>
 
      _fruit.TryGetValue(id, out var fruit)
          ? TypedResults.Ok(fruit)
-         : Results.Problem(statusCode: 404))
-         .AddEndpointFilter<IdValidationFilter>();
+         : Results.Problem(statusCode: 404));
 
-app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+fruitGroup.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
     _fruit.TryAdd(id, fruit)
         ? TypedResults.Created($"/fruit/{id}", fruit)
         : Results.ValidationProblem(new Dictionary<string, string[]>
         {
             { "id", new[] { "Fruit with this ID already exists." } }
-        })).AddEndpointFilter<IdValidationFilter>();
+        }));
 
-app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+fruitGroup.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
 {
     _fruit[id] = fruit;
     return Results.NoContent();
 });
 
-app.MapDelete("/fruit/{id}", (string id) =>
+fruitGroup.MapDelete("/fruit/{id}", (string id) =>
 {
     _fruit.TryRemove(id, out var removedFruit);
     return Results.NoContent();
